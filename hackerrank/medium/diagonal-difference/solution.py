f677b894b530b0e56f82fdd03d8d5a5bd25194d6
bhavanakor <150333705+bhavanakor@users.noreply.github.com>
2026-09-11 21:07:31 +0530

    ✅ Diagonal Difference | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ hackerrank/medium/diagonal-difference/solution.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    left_diagonal=0
+    right_diagonal=0
+    l=len(arr)
+    for i in range(l):
+        left_diagonal+=arr[i][i]
+        right_diagonal+=arr[i][l-(i+1)]
+    return abs(left_diagonal-right_diagonal)
+    
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
